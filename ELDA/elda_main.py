507812a0a5dd8b87c769100c86ed5650721e7540
HYEONGSOOPC\hyeongsoo <hyeongsoo.kim@m2utech.com>
2017-01-04 21:50:53 +0900

    2017.01.04

--- ELDA/elda_main.py
+++ ELDA/elda_main.py
@@ -5,11 +5,12 @@ import pandas.io.sql as pd_sql
 from pandas import DataFrame
 import datetime as dt
 from math import sqrt
+import json
 
 ### required eyelink modules ###
-import elda_parstream_conn as elda_pc
+#import elda_parstream_conn as elda_pc
 import elda_extract_data as elda_ed
-import elda_read_csv as elda_rc
+#import elda_read_csv as elda_rc
 import elda_preprocessing as elda_pre
 
 
@@ -19,85 +20,44 @@ import matplotlib.pyplot as plt
 
 from pandasql import sqldf
 
+###### load json ######
+#rawdata = pd.read_json('C:\\test.json', typ='series') #series형태로 로드시
+rawdata = pd.read_json('C:\\test.json', typ='frame') #typ's default -> frame
 
-##### parstream connection using pandas #####
-sql = """
-SELECT * 
-FROM tb_node_raw 
-WHERE event_time >= TIMESTAMP'2016-11-16 00:00:00' and event_time <= TIMESTAMP'2016-11-20 23:59:59' 
-"""
-targetdata = pd_sql.read_sql(sql,elda_pc.conn,index_col='event_time')
-#WHERE event_type = 1
-#ORDER BY event_time;
-
-
-
-elda_pc.conn.close() #closed conn
-
-print(targetdata)
-
-import pdb; pdb.set_trace()  # breakpoint 71147eb1 //
-
-"""
-##### csv파일 열기 #####
-dataset = elda_rc.read_file()	#read_csvfile module
-
-# 원하는 기간 설정: test 11.18 ~ 11.18
-targetdata = dataset
-targetdata.index = targetdata.event_time
-
-#st = dt.datetime(2016,11,18,0,0,0)
-#en = dt.datetime(2016,12,13,10,30)
-#targetdata = targetdata.ix[st:en]
-
-targetdata = targetdata.ix[targetdata.index.min():targetdata.index.max()]
-"""
+#print(rawdata)
 
 #원하는 데이터 속성 추출 피벗 및 클러스터링 
-voltage_data = DataFrame(targetdata, columns=['node_id', 'event_time', 'voltage'])
-#ampere_data = DataFrame(targetdata, columns=['node_id', 'event_time', 'ampere'])
-#vib_x_data = DataFrame(targetdata, columns=['node_id', 'event_time', 'vibration_x'])
-#vib_y_data = DataFrame(targetdata, columns=['node_id', 'event_time', 'vibration_y'])
-#vib_z_data = DataFrame(targetdata, columns=['node_id', 'event_time', 'vibration_z'])
+voltage_data = DataFrame(rawdata, columns=['node_id', 'event_time', 'voltage'])
 
 # function (data, index, columns, values, default_value)
 voltage_data = elda_ed.extract_data(voltage_data, 'event_time', 'node_id', 'voltage', 200)
-#ampere_data = elda_ed.extract_data(ampere_data, 'event_time', 'node_id', 'ampere', 0)
-#vib_x_data = elda_ed.extract_data(vib_x_data, 'event_time', 'node_id', 'vibration_x', 0)
-#vib_y_data = elda_ed.extract_data(vib_y_data, 'event_time', 'node_id', 'vibration_y', 0)
-#vib_z_data = elda_ed.extract_data(vib_z_data, 'event_time', 'node_id', 'vibration_z', 0)
 
 #print(len(voltage_data.columns)) #number of columns
 
-voltage_data = voltage_data.T
+#voltage_data = voltage_data.T
 
+#voltage_data = voltage_data.values.tolist()
 
-voltage_data = voltage_data.values.tolist()
-print(voltage_data)
-
-import pdb; pdb.set_trace()  # breakpoint 02de47f7 //
 
 #voltage_data = pd.DataFrame(voltage_data.values)
-
-
-#test = voltage_data.tolist()
+voltage_data = voltage_data.unstack()
 
 print(voltage_data)
 
-import pdb; pdb.set_trace()  # breakpoint cd40a9db //
+print(type(voltage_data))
 
-import ts_cluster
+#import ts_cluster
 
-ts_cluster.k_means_clust(voltage_data,5,10,4)
+#ts_cluster.k_means_clust(voltage_data,5,10,4)
 
 
-ts={}
+#ts={}
 
-for i in range(len(voltage_data.columns)):
-	ts[i] = voltage_data.ix[:,i]
+#for i in range(len(voltage_data.columns)):
+#	ts[i] = voltage_data.ix[:,i]
 	#ts[i].plot()
 
-print(ts)
+#print(ts)
 #plt.legend(prop={'size':5})
 #plt.show()
 
