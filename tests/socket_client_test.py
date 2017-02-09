477644d1a6bfd0b37ff226735384d9fb9d1e4dd6
HYEONGSOOPC\hyeongsoo <hyeongsoo.kim@m2utech.com>
2017-02-09 21:04:50 +0900

    2017.02.09

--- tests/socket_client_test.py
+++ tests/socket_client_test.py
@@ -4,16 +4,18 @@
 
 import socket
 
-
+#HOST = 'm2u-da.eastus.cloudapp.azure.com'
+#HOST = '40.117.230.68'
+PORT=5225 #포트지정
 HOST='192.168.10.27' #localhost
-
-PORT=5225 #서버와 같은 포트사용
+#HOST = 'localhost'
+#PORT=5225 #서버와 같은 포트사용
 
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #소켓생성
 
 s.connect((HOST,PORT))
 
-s.send(b'{"start_date": "2016-12-01", "end_date": "2016-12-07", "time_interval": 15}') #문자를 보냄
+s.send(b'{"start_date": "2016-11-22", "end_date": "2016-11-22", "time_interval": "15"}') #문자를 보냄
 
 data = s.recv(1024) #서버로 부터 정보를 받음
 
