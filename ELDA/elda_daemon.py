e3fd92b3ec52066b716c3dad701c74ba43b385ed
HYEONGSOOPC\hyeongsoo <hyeongsoo.kim@m2utech.com>
2017-02-28 20:06:29 +0900

    2017.2.28

--- ELDA/elda_daemon.py
+++ ELDA/elda_daemon.py
@@ -20,14 +20,14 @@ def start_daemon():
 	logger = logging.getLogger("DaemonLog")
 	logger.setLevel(logging.INFO)
 	formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-	handler = logging.FileHandler("/home/Toven/da/logs/elda_daemon.log")
+	handler = logging.FileHandler(cfg_default['logging_path'])
 	handler.setFormatter(formatter)
 	logger.addHandler(handler)
 
 	daemon_context = daemon.DaemonContext(
 		working_directory='/home/Toven/da/elda',
         umask=0o002,
-        pidfile=daemon.pidfile.PIDLockFile('/home/Toven/da/elda_daemon.pid'),
+        pidfile=daemon.pidfile.PIDLockFile(cfg_default['pidfile_path'),
         files_preserve=[handler.stream]
 	)
 
