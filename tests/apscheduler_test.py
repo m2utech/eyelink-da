085446ee9eaab6dbe05f0afbcf6d8c732e5f1d87
HYEONGSOOPC\hyeongsoo <hyeongsoo.kim@m2utech.com>
2017-03-17 20:27:25 +0900

    2017.03.17

--- tests/apscheduler_test.py
+++ tests/apscheduler_test.py
@@ -1,13 +1,34 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+import socket
+import time
 
+import configparser
+
+# 전역변수로 처리 필요
+config = configparser.ConfigParser()
+config.read('../config.cfg')
+cfg_server = config['SERVER_INFO']
+cfg_default = config['DEFAULT_INFO']
 
 def job_function():
-     print ("Hello World")
+	HOST = 'm2u-da.eastus.cloudapp.azure.com'
+	PORT = 5225
+	print("Start data analysis")
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #소켓생성
+	s.connect((HOST,PORT))
+	s.send(b'{"start_date": "2017-02-08", "end_date": "2017-02-08", "time_interval": 60}') #문자를 보냄
+	data = s.recv(1024) #서버로 부터 정보를 받음
+	s.close()
+	print('Received',repr(data))
 
-sched = BlockingScheduler()
-
-# Schedules job_function to be run on the third Friday
-# of June, July, August, November and December at 00:00, 01:00, 02:00 and 03:00
-sched.add_job(job_function, 'cron', day_of_week='mon-fri', second='*/3')
-
-sched.start()
+def run():
+	print("test")
+	sched = BlockingScheduler()
+	# Schedules job_function to be run on the third Friday
+	# of June, July, August, November and December at 00:00, 01:00, 02:00 and 03:00
+	#sched.add_job(job_function, 'cron', day_of_week='mon-fri', minute='*/1')
+	sched.add_job(job_function, 'cron', day_of_week='fri-sun', hour=1)
+	sched.start()
+	
+if __name__ == '__main__':
+	run()
--- tests/elda_daemon_test.py
+++ tests/elda_daemon_test.py
@@ -2,14 +2,10 @@
 # PYTHONPATH=`pwd` python elda_daemon.py start
 
 #standard python libs
-import logging
 import time
 
 #third party libs
-import daemon
 from daemon import runner
-from elda_main import socket_server
-
 
 class App():
   def __init__(self):
@@ -21,24 +17,10 @@ class App():
 
   def run(self):
     while True:
-      logger.debug("Debug message")
-      logger.info("Info message")
-      logger.warn("Warning message")
-      logger.error("Error message")
+      print("tetetettetet")
       time.sleep(10)
 
 app = App()
-logger = logging.getLogger("DaemonLog")
-logger.setLevel(logging.INFO)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler = logging.FileHandler("/home/Toven/da/logs/elda_daemon.log")
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
 daemon_runner = runner.DaemonRunner(app)
 
-#This ensures that the logger file handle does not get closed during daemonization
-daemon_runner.daemon_context.files_preserve=[handler.stream]
-with daemon.DeamonContext():
-  socket_server()
-#  daemon_runner.do_action()
+daemon_runner.do_action()
