74b8d1d21ba6a7cd19a262689cc7c1f620cd9077
HYEONGSOOPC\hyeongsoo <hyeongsoo.kim@m2utech.com>
2017-03-29 19:35:20 +0900

    2017.03.29

--- ELDA/elda_main.py
+++ ELDA/elda_main.py
@@ -5,6 +5,10 @@ import json
 import configparser
 import elda_clustering as clustering_main
 
+import logging
+import logging.handlers
+import time
+
 #configuration
 import config_info as config
 
@@ -18,31 +22,48 @@ class ClientThread(Thread):
 		print("New da-server socket thread started for" + ip + ":" + str(port))
 
 	def json_parsing(data):
+
+		# running time check
+		start_time = time.time()
+		logger.info("===== start data analysis =====")
+
 		dict = json.loads(data.decode("utf-8")) # dictionary type
 		start_date = dict['start_date']
-		print(start_date)
-		print(type(start_date))
 		end_date = dict['end_date']
-		print(end_date)
-		print(type(end_date))
 		time_interval = dict['time_interval']
-		print(time_interval)
-		print(type(time_interval))
 		clustering_main.data_load(start_date, end_date, time_interval)
+
+		running_time = time.time() - start_time
+		logger.info("Start-date:{0} | End-date:{1} | Time-interval:{2} >> Running-time:{3:.03f} sec".format(start_date,end_date,time_interval, running_time))
+
+		
 
 
 	def run(self):
 		while True:
 			data = conn.recv(2048)
 			print("Server received data:", data)
-
 			if not data:	break
-
 			ClientThread.json_parsing(data)
-
 			conn.send(b"analysis success")
 			print("anlysis success")
 
+
+#########################################
+# make logger instance
+logger = logging.getLogger("Running_Log")
+logger.setLevel(logging.INFO)
+
+# make formatter
+formatter = logging.Formatter('[%(levelname)s|%(asctime)s] > %(message)s')
+
+# make handler to output Log for stream and file
+fileMaxByte = 1024 * 1024 * 100 #100MB
+fileHandler = logging.handlers.RotatingFileHandler(config.cfg['run_log_path'], maxBytes=fileMaxByte, backupCount=10)
+# specify formatter to each handler
+fileHandler.setFormatter(formatter)
+# attach stream and file handler to logger instance
+logger.addHandler(fileHandler)
 
 #HOST = '192.168.10.27'
 #PORT=5225 #포트지정
--- tests/socket_client_test.py
+++ tests/socket_client_test.py
@@ -11,11 +11,12 @@ PORT=5225 #포트지정
 #HOST = 'localhost'
 #PORT=5225 #서버와 같은 포트사용
 
+
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) #소켓생성
 
 s.connect((HOST,PORT))
 
-s.send(b'{"start_date": "2017-02-05", "end_date": "2017-02-05", "time_interval": 30}') #문자를 보냄
+s.send(b'{"start_date": "2017-01-01", "end_date": "2017-01-31", "time_interval": 60}') #문자를 보냄
 
 data = s.recv(2048) #서버로 부터 정보를 받음
 
