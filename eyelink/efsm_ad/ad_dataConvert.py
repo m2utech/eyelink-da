9b852bf9a98abbff9713f896ae7d125eea85d494
HYEONGSOOPC\hyeongsoo <hyeongsoo.kim@m2utech.com>
2017-12-21 11:33:54 +0900

    폴더정리

---
+++ eyelink/efsm_ad/ad_dataConvert.py
@@ -0,0 +1,73 @@
+import requests
+import json
+import pandas as pd
+import consts
+# configuration
+from ad_configParser import getConfig
+cfg = getConfig()
+
+############################
+def loadJsonData(node_id, s_date, e_date, cfg):
+    query = "?nodeId={}&startDate={}&endDate={}".format(node_id, s_date, e_date)
+    url = cfg['API']['url_get_train_data']
+
+    resp = requests.get(url + query)
+    dataset = json.loads(resp.text)
+    if dataset['rtnCode']['code'] == '0001':
+        dataset = None
+    else:
+        attr = list(consts.FACTOR_INFO['FACTORS'].keys())
+        ind = []
+        ind.append(consts.FACTOR_INFO['INDEX'])
+        dataset = pd.DataFrame(dataset['rtnData'], columns=attr+ind)
+        dataset[ind[0]] = pd.to_datetime(dataset[ind[0]], format='%Y-%m-%d %H:%M:%S.%f')
+        for factor in attr:
+            dataset[factor] = dataset[factor].apply(pd.to_numeric, errors='ignore')
+
+        dataset = dataset.set_index(ind[0])
+
+    return dataset
+############################
+
+def loadPatternInfo(id):
+    url = cfg['API']['url_get_pattern_info'] + "?id={}".format(id)
+    resp = requests.get(url)
+    dataset = json.loads(resp.text)
+
+    if dataset['rtnCode']['code'] == '0001':
+        dataset = None
+    else:
+        dataset = dataset['rtnData']
+
+    return dataset
+
+############################
+def loadPatternData(id):
+    url = cfg['API']['url_get_pattern_data'] + id
+    resp = requests.get(url)
+    dataset = json.loads(resp.text)
+
+    if dataset['rtnCode']['code'] == '0001':
+        dataset = None
+    else:
+        dataset = dataset['rtnData']
+
+    return dataset
+
+
+def processResamplingAndMissingValue(dataset, def_val, t_interval, output):
+    dataset = dataset.resample(str(t_interval)+'T').mean()
+    dataset = dataset.fillna(def_val)
+    dataset = dataset.reset_index()
+    output.put(dataset)
+
+
+def extractAttribute(dataset, attr):
+    data = dataset[attr]
+    return data
+
+
+if __name__ == "__main__":
+    # dataset = loadJsonData('0002.00000039', '2017-11-28T00:00:00Z', '2017-11-29T02:00:00Z', cfg)
+    dataset = loadPatternInfo('master')
+    print(dataset)
---
+++ eyelink/efsm_ca/ca_scheduler.py
@@ -0,0 +1,108 @@
+import sys
+from daemon import Daemon
+from ca_logger import getCaLogger
+import socket
+import consts
+import util
+from apscheduler.schedulers.background import BackgroundScheduler
+
+host = consts.HOST
+port = consts.PORT
+
+
+class Scheduler(object):
+    def __init__(self):
+        self.sched = BackgroundScheduler()
+        self.sched.start()
+
+    # 클래스가 종료될때, 모든 job들을 종료시켜줍니다.
+    def __del__(self):
+        self.shutdown()
+
+    # 모든 job들을 종료시켜주는 함수입니다.
+    def shutdown(self):
+        self.sched.shutdown()
+
+    def job_day(self):
+        logger.info("===== Start cluster analysis for one day =====")
+        s_date, e_date = util.getTimeRangeByDay(consts.TIME_RANGE['DAY'], consts.DATETIME)
+        sendData = {"sDate": s_date, "eDate": e_date, "tInterval": consts.TIME_INTERVAL['DAY']}
+        sendData = str(sendData).encode()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 소켓생성
+        s.connect((host, port))
+        s.send(sendData)
+        s.close()
+
+    def job_week(self):
+        logger.info("===== Start cluster analysis for one week =====")
+        s_date, e_date = util.getTimeRangeByWeek(consts.TIME_RANGE['WEEK'], consts.DATETIME)
+        sendData = {"sDate": s_date, "eDate": e_date, "tInterval": consts.TIME_INTERVAL['WEEK']}
+        sendData = str(sendData).encode()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 소켓생성
+        s.connect((host, port))
+        s.send(sendData)
+        s.close()
+
+    def job_month(self):
+        logger.info("===== Start cluster analysis for one month =====")
+        s_date, e_date = util.getTimeRangeByMonth(consts.TIME_RANGE['MONTH'], consts.DATETIME)
+        sendData = {"sDate": s_date, "eDate": e_date, "tInterval": consts.TIME_INTERVAL['MONTH']}
+        sendData = str(sendData).encode()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # 소켓생성
+        s.connect((host, port))
+        s.send(sendData)
+        s.close()
+
+    def job_test(self):
+        logger.info("===== test check 30 minute =====")
+
+    def scheduler(self):
+        self.sched.add_job(
+            self.job_day, "cron", max_instances=consts.SCHEDULER['MAX_INSTANCE'],
+            hour=consts.SCHEDULER['HOUR']
+        )
+        self.sched.add_job(
+            self.job_week, "cron", max_instances=consts.SCHEDULER['MAX_INSTANCE'],
+            day_of_week=consts.SCHEDULER['DAY_OF_WEEK']
+        )
+        self.sched.add_job(
+            self.job_month, "cron", max_instances=consts.SCHEDULER['MAX_INSTANCE'],
+            day=consts.SCHEDULER['DAY']
+        )
+        self.sched.add_job(
+            self.job_test, "cron", max_instances=consts.SCHEDULER['MAX_INSTANCE'],
+            minute=30)
+
+
+class SchedulerDaemon(Daemon):
+
+    def run(self):
+        scheduler = Scheduler()
+        scheduler.scheduler()
+
+        while True:
+            pass
+
+
+if __name__ == '__main__':
+    logger = getCaLogger()
+    print(consts.PATH["SCHE_PID"])
+    daemon = SchedulerDaemon(consts.PATH["SCHE_PID"])
+
+    if len(sys.argv) == 2:
+        if "start" == sys.argv[1]:
+            logger.info("Started Cluster Analysis Scheduler ...")
+            daemon.start()
+        elif "stop" == sys.argv[1]:
+            logger.info("Stopped Cluster Analysis Scheduler ...")
+            daemon.stop()
+        elif "restart" == sys.argv[1]:
+            logger.info("Restarted Cluster Analysis Scheduler ...")
+            daemon.restart()
+        else:
+            print("unknown command")
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print("usage: %s start|stop|restart" % sys.argv[0])
+        sys.exit(2)
